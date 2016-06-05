068de5f5bf73eed8f1a3008cb9ba3906a6c8999e
axl812 <axl812@gmail.com>
2016-06-05 16:23:10 +0300

    Prase orders input

--- app.rb
+++ app.rb
@@ -26,7 +26,6 @@ get '/' do
 end
 
 post '/' do
-	
 	erb :cart
 end
 
@@ -34,13 +33,29 @@ get '/about' do
 	erb :about
 end
 
-get '/cart' do
-	@orders = params[:orders]
-	
-	erb :cart
+post '/cart' do
+	orders_input = params[:orders]
+	@orders = parse_orders_input orders_input
+	erb "Hello #{@orders.inspect}"
 end
 
-post '/cart' do
+def parse_orders_input orders_input
 
-	erb :cart
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split('=')
+
+		s3 = s2[0].split(/\_/)
+				
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]
+		arr.push arr2
+
+	end
+	return arr
 end
